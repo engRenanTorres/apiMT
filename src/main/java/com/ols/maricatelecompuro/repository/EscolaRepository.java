56010521bee17c640e88e7b636984cda7d1c2a52
OLSRenanSouza <106403510+OLSRenanSouza@users.noreply.github.com>
2022-08-15 09:15:18 -0300

    Update EscolaRepository
    Correção no padrão do nome da função findBy

--- src/main/java/com/ols/maricatelecompuro/repository/EscolaRepository.java
+++ src/main/java/com/ols/maricatelecompuro/repository/EscolaRepository.java
@@ -4,5 +4,5 @@ import com.ols.maricatelecompuro.model.Escola;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EscolaRepository extends JpaRepository<Escola, Integer> {
-  Escola findByNome(String nomeEscola);
+  Escola findByNomeEscola(String nomeEscola);
 }
